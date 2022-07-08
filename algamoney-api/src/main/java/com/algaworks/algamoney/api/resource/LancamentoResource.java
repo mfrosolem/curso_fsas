dd414313c3c2715b580edef6aee69a2a04d6ab18
Maira Francini <mairafrancini@gmail.com>
2022-07-08 13:46:05 -0300

    Início do controle de versão

---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,92 @@
+package com.algaworks.algamoney.api.resource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
+import com.algaworks.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
+import com.algaworks.algamoney.api.model.Lancamento;
+import com.algaworks.algamoney.api.repository.LancamentoRepository;
+import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
+import com.algaworks.algamoney.api.service.LancamentoService;
+import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	private LancamentoService lancamentoService;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@GetMapping
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return this.lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
+		return this.lancamentoRepository.findById(codigo).map(lancamento -> ResponseEntity.ok(lancamento))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Lancamento> salvar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = this.lancamentoService.salvar(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+
+	@DeleteMapping("/{codigo}")
+	public ResponseEntity<Lancamento> remover(@PathVariable Long codigo) {
+		Lancamento lancamento = lancamentoRepository.getById(codigo);
+
+		if (lancamento != null) {
+			lancamentoRepository.delete(lancamento);
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
+		return ResponseEntity.badRequest().body(erros);
+	}
+
+}
